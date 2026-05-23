037cbf67c6866eebf243876619745a77ddc039d5
vasanth <vasanthudesh@gmail.com>
2026-05-23 19:54:32 +0530

    main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
+import { RegisterComponent } from './register/register.component';
+import { LiveTrackingComponent } from './live-tracking/live-tracking.component';
+import { BookingComponent } from './booking/booking.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), provideHttpClient()],
-});
+export const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'support', component: SupportComponent },
+  { path: 'live-tracking', component: LiveTrackingComponent },
+  { path: 'booking', component: BookingComponent },
+];
